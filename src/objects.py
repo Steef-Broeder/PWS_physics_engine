5b4874272ecf7203a7385f8496db58e2b1713aaa
Steef Broeder <steef@broeder.co.nl>
2022-11-23 16:22:38 +0100

    changes

--- src/objects.py
+++ src/objects.py
@@ -35,57 +35,27 @@ import pygame
 #         pygame.draw.circle(surface=self.surface, color=self.color, center=(self.x, self.y), radius=self.radius)
 
 
-# class object:
-#     def __init__(self, surface:pygame.Surface, x:int, y:int, color:tuple) -> None:
-#         self.surface = surface
-#         self.x = x
-#         self.y = y
-#         self.color = color
+class object:
+    def __init__(self, surface:pygame.Surface, x:int, y:int, color:tuple) -> None:
+        self.surface = surface
+        self.x = x
+        self.y = y
+        self.color = color
 
-#     def set_position(self, x, y):
-#         self.x = x
-#         self.y = y
-
-#     def set_color(self, color:tuple):
-#         self.color = color
-
-# class rect(object):
-#     def __init__(self, surface:pygame.Surface, x: int, y: int, color: tuple, width:int, height:int) -> None:
-#         super().__init__(surface, x, y, color)
-#         self.width = width
-#         self.height = height
-
-#     def draw(self):
-#         rectangle = pygame.Rect(self.x, self.y, self.width, self.height)
-#         pygame.draw.rect(surface=self.surface, color=self.color, rect=rectangle)
-
-
-# class circle(object):
-#     def __init__(self, surface:pygame.Surface, x: int, y: int, color: tuple, radius:int) -> None:
-#         super().__init__(surface, x, y, color)
-#         self.radius = radius
-	
-#     def draw(self):
-#         pygame.draw.circle(surface=self.surface, color=self.color, center=(self.x, self.y), radius=self.radius)
-
-class circle():
+class circle(object):
 	def __init__(self, surface:pygame.Surface, x:int, y:int, color:tuple, radius:int) -> None:
-		self.surface = surface
-		self.x = x
-		self.y = y
-		self.color = color
+		super().__init__(surface, x, y, color)
 		self.radius = radius
+
 	def draw(self):
 		pygame.draw.circle(surface=self.surface, color=self.color, center=(self.x, self.y), radius=self.radius)
 
 class rect(object):
 	def __init__(self, surface:pygame.Surface, x:int, y:int, color:tuple, width:int, height:int) -> None:
-		self.surface = surface
-		self.x = x
-		self.y = y
-		self.color = color
+		super().__init__(surface, x, y, color)
 		self.width = width
 		self.height = height
+
 	def draw(self):
 		rectangle = pygame.Rect(self.x, self.y, self.width, self.height)
 		pygame.draw.rect(surface=self.surface, color=self.color, rect=rectangle)
