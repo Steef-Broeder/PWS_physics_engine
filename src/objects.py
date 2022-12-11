117c4db9f796343b43694cced7fa7850c2dc128e
Steef Broeder <steef@broeder.co.nl>
2022-12-11 12:41:51 +0100

    changes

--- src/objects.py
+++ src/objects.py
@@ -1,15 +1,14 @@
 import pygame
 
 class object:
-	def __init__(self, surface:pygame.Surface, x:int, y:int, color:tuple[int, int, int], mass:float = 5, velocity:tuple[int, int] = (0, 0), force:tuple[int, int] = (0, 0), acceleration_variable:int = 9.81) -> None:
+	def __init__(self, surface:pygame.Surface, x:int, y:int, color:tuple[int, int, int], mass:float = 5, velocity:tuple[int, int] = (0, 0), acceleration:tuple[float, float] = (0, 0)) -> None:
 		self.surface = surface
 		self.x = x
 		self.y = y
 		self.color = color
 		self.mass = mass
 		self.velocity = velocity
-		self.force = force
-		self.acceleration_variable = acceleration_variable
+		self.acceleration = acceleration 
 
 	def set_color(self, color:tuple[int, int, int]) -> None:
 		self.color = color
@@ -21,24 +20,25 @@ class object:
 		self.x = x
 		self.y = y
 
-	def get_aantrekkingskracht(self):
-		self.aantrekkingskracht = self.mass * self.acceleration_variable
+	def get_applied_force(self):
+		self.applied_force = self.mass * self.acceleration_variable
+		return self.applied_force
 
-	def update(self, deltatime:float) -> None:
-		self.velocity = (((self.force[0]/self.mass) * deltatime) + self.velocity[0], ((self.force[1]/self.mass) * deltatime) + self.velocity[1])
+	def update(self, force:tuple(float, float), deltatime:float) -> None:
+		self.velocity = (((force[0]/self.mass) * deltatime) + self.velocity[0], ((force[1]/self.mass) * deltatime) + self.velocity[1])
 
 
 class circle(object):
-	def __init__(self, surface:pygame.Surface, x:int, y:int, color:tuple[int, int, int], mass:float = 5, velocity:tuple[int, int] = (0, 0), force:tuple[int, int] = (0, 0), radius:float = 10) -> None:
-		super().__init__(surface, x, y, color, mass, velocity, force)
+	def __init__(self, surface:pygame.Surface, x:int, y:int, color:tuple[int, int, int], mass:float = 5, velocity:tuple[int, int] = (0, 0), acceleration:tuple[float, float] = (0, 0), radius:float = 10) -> None:
+		super().__init__(surface, x, y, color, mass, velocity, acceleration)
 		self.radius = radius
 
 	def draw(self):
 		pygame.draw.circle(surface=self.surface, color=self.color, center=(self.x, self.y), radius=self.radius)
 
 class rect(object):
-	def __init__(self, surface:pygame.Surface, x:int, y:int, color:tuple[int, int, int], mass:float = 5, velocity:tuple[int, int] = (0, 0), force:tuple[int, int] = (0, 0), width:int = 10, height:int = 10) -> None:
-		super().__init__(surface, x, y, color, mass, velocity, force)
+	def __init__(self, surface:pygame.Surface, x:int, y:int, color:tuple[int, int, int], mass:float = 5, velocity:tuple[int, int] = (0, 0), acceleration:tuple[float, float] = (0, 0), width:int = 10, height:int = 10) -> None:
+		super().__init__(surface, x, y, color, mass, velocity, acceleration)
 		self.width = width
 		self.height = height
 
